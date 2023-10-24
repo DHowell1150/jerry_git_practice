27011c13ded6fd3f2ae6021f07b858313d003e02
Dana Howell <howelld115@gmail.com>
2023-10-24 15:14:40 -0600

    Initialize student in student file

--- student.rb
+++ student.rb
@@ -6,3 +6,13 @@
 
 # Get their name
 
+class Student
+  
+  @first_name = ""
+  @last_name = ""
+  
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name    
+  end
+end
