014d722ca02d4210b1d8160334e6dda97b60fb8d
Dana Howell <howelld115@gmail.com>
2023-10-24 19:31:22 -0600

    Student name functinality completed

--- student.rb
+++ student.rb
@@ -1,18 +1,21 @@
 # Student object
-
 #Create a student
-
 # Set their first and last name
-
 # Get their name
 
 class Student
   
   @first_name = ""
   @last_name = ""
-  
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name = last_name    
   end
+
+  def name
+    return "#{first_name} #{last_name}" 
+  end
+
 end
+
